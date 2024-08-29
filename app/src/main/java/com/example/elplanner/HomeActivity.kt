4dd072908d0a5ae98b1c7307d98a45dd5d68a489
Cole-showers7 <98387486+Cole-showers7@users.noreply.github.com>
2024-08-29 10:51:31 +0100

    Added AddTask popup, next, icons for time, date and priority

--- app/src/main/java/com/example/elplanner/HomeActivity.kt
+++ app/src/main/java/com/example/elplanner/HomeActivity.kt
@@ -5,23 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,6 +43,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -63,19 +76,25 @@ class HomeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Index(){
+fun Index() {
     val navController = rememberNavController()
 
-    Column (
-        modifier = Modifier.fillMaxSize(0.9f)
-    ){
-        HomePage()
-        NavHost(navController = navController, startDestination = "EmptyPage") {
-            composable("EmptyPage") { EmptyPage() }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            HomePage()
+            NavHost(navController = navController, startDestination = "EmptyPage") {
+                composable("EmptyPage") { EmptyPage() }
+                composable("AddTask"){ AddTask()}
+            }
         }
-        BottomBar()
+        BottomBar(navController)
     }
 }
+
 @Composable
 fun HomePage() {
     val menu = painterResource(id = R.drawable.menu)
@@ -87,18 +106,17 @@ fun HomePage() {
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically, // Align items vertically centered
-            horizontalArrangement = Arrangement.SpaceBetween // Distribute space between items
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
                 painter = menu,
                 contentDescription = null,
                 modifier = Modifier
                     .size(42.dp)
-                    .align(Alignment.CenterVertically) // Align image vertically centered
+                    .align(Alignment.CenterVertically)
             )
 
-//            Spacer(modifier = Modifier.weight(1f)) // Take up the space between image and text
 
             Text(
                 text = "Index",
@@ -107,7 +125,7 @@ fun HomePage() {
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Medium
                 ),
-                modifier = Modifier.align(Alignment.CenterVertically) // Align text vertically centered
+                modifier = Modifier.align(Alignment.CenterVertically)
             )
         }
     }
@@ -153,26 +171,134 @@ fun EmptyPage(){
 }
 
 @Composable
-fun BottomBar() {
-    val items = listOf("Home", "Tasks", "Settings")
-    val selectedIndex = remember { mutableStateOf(0) }
+fun BottomBar(navController: NavController) {
+    val items = listOf("Index", "Calendar", "Focus", "Profile")
+    val icons = mapOf(
+        "Index" to R.drawable.indexicon,
+        "Calendar" to R.drawable.calendaricon,
+        "Focus" to R.drawable.focusicon,
+        "Profile" to R.drawable.profileicon
+    )
 
-    BottomNavigation(
-        backgroundColor = Color.Gray,
-        contentColor = Color.White
+    val selectedIndex = remember { mutableIntStateOf(0) }
+
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+
     ) {
-        items.forEachIndexed { index, item ->
-            BottomNavigationItem(
-                icon = {
-                    Icon(
-                        painter = painterResource(id = R.drawable.menu), // Replace with your icons
-                        contentDescription = item
-                    )
+        BottomNavigation(
+            backgroundColor = Color(0xFF363636),
+            contentColor = Color.White,
+            modifier = Modifier
+                .height(114.dp)
+                .padding(top = 12.dp)
+        ) {
+            items.forEachIndexed { index, item ->
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = icons[item] ?: R.drawable.menu),
+                            contentDescription = item,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    },
+                    label = {
+                        Text(
+                            item,
+                            color = Color.White,
+                            style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Light)
+                        )
+                    },
+                    selected = selectedIndex.intValue == index,
+                    onClick = { selectedIndex.intValue = index }
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = (-36).dp)
+                .size(64.dp)
+                .background(Color(0xFF8875FF), shape = CircleShape)
+                .clickable {
+                    navController.navigate("AddTask")
                 },
-                label = { Text(item) },
-                selected = selectedIndex.value == index,
-                onClick = { selectedIndex.value = index }
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.addicon),
+                contentDescription = "Add",
+                tint = Color.White,
+                modifier = Modifier.size(32.dp)
             )
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTask() {
+    var task by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp)
+                .background(Color(0xFF363636))
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Add Task",
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                ),
+                modifier = Modifier.padding(bottom = 12.dp)
+            )
+            OutlinedTextField(
+                value = task,
+                onValueChange = { task = it },
+                label = { Text("Task") },
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    focusedBorderColor = Color(0xFF8875FF),
+                    unfocusedBorderColor = Color.White
+                ),
+                textStyle = TextStyle(
+                    color = Color.White
+                )
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text("Description (Optional)") },
+                modifier = Modifier
+                    .fillMaxWidth(0.9f),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedLabelColor = Color.White,
+                    unfocusedLabelColor = Color.White,
+                    focusedBorderColor = Color(0xFF8875FF),
+                    unfocusedBorderColor = Color.White
+                ),
+                textStyle = TextStyle(
+                    color = Color.White
+                )
+            )
+
         }
     }
 }
